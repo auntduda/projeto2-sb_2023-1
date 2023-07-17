ec4611bedd4eb16705b78c5a18c181e1b5e213e2
Maria Eduarda Carvalho <57044209+auntduda@users.noreply.github.com>
2023-07-17 12:16:27 -0300

    Update DIV.asm - test later

--- src/DIV.asm
+++ src/DIV.asm
@@ -1,5 +1,80 @@
 section .text
-extern aux_str, tamanho, print32, print16, read32, read16
-global div
+; dados externos
+extern aux_str, tamanho, msg_numero, msg_resultado, msg_overflow
+; funcoes externas
+extern print32, print16, printstr, read32, read16, exit
 
-div: ret
+; funcoes publicas
+global div
+div:    ; se tamanho = 0, vai para divisao de 16 bits
+        cmp byte [tamanho], 0
+        je div16
+        ; 32 bits
+        ; comeca o frame de pilha com 4 bytes reservados para uma variavel local
+        enter 4, 0
+
+        push msg_numero
+        call printstr
+        ; ler numero usuario
+        push aux_str
+        call read32
+        ; guarda o primeiro numero como variavel local
+        mov [ebp-4], eax
+
+        push msg_numero
+        call printstr
+        ; ler segundo numero
+        push aux_str
+        call read32
+
+        ; divide os dois numeros
+        idiv dword [ebp-4]
+        jo div_of
+
+        ; imprime resultado
+        push msg_resultado
+        call printstr
+        ; printa resultado de 32 bits
+        push eax
+        push aux_str
+        call print32
+    
+        leave
+        ret
+
+        ; 16 bits
+div16:  ; comeca o frame de pilha com 2 bytes reservados para uma variavel local
+        enter 2, 0
+        
+        push msg_numero
+        call printstr
+        ; ler numero usuario
+        push aux_str
+        call read16
+        ; guarda o primeiro numero como variavel local
+        mov [ebp-2], ax
+
+        push msg_numero
+        call printstr
+        ; ler segundo numero
+        push aux_str
+        call read16
+
+        ; divide os dois numeros
+        idiv word [ebp-2]
+        jo div_of
+
+        ; imprime resultado
+        push msg_resultado
+        call printstr
+        ; printa resultado de 32 bits
+        push ax
+        push aux_str
+        call print16
+    
+        leave
+        ret
+
+div_of: push msg_overflow
+        call printstr
+        jmp exit
