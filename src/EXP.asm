4481beb675776e21ed24116b96efd3151bb62788
Maria Eduarda Carvalho <57044209+auntduda@users.noreply.github.com>
2023-07-18 09:42:18 -0300

    Update EXP.asm
    consertando o babado do registrador mas essa inhaca ainda ta quebrada - faz +1 operacao do que deveria antes de acabar

--- src/EXP.asm
+++ src/EXP.asm
@@ -5,8 +5,6 @@ extern aux_str, tamanho, msg_numero, msg_resultado, msg_overflow, mul
 extern print32, print16, printstr, read32, read16, exit
 global exp
 
-;;;;;;;;;;;;;;;;; TA TUDO ERRADO PROQUE ESQUECI QUE OPERACAO DE MULTIPLICACAO E DIVISAO SO OPERA COM REGISTRADOR AAAAAAAAAAAAAA
-
 exp:        ;se tamanho = 0, vai para divisao de 16 bits
             cmp byte [tamanho], 0
             je exp16
@@ -19,29 +17,28 @@ exp:        ;se tamanho = 0, vai para divisao de 16 bits
             ; ler numero usuario
             push aux_str
             call read32
-            ; colocando o primeiro numero em ebx para trocar os dados de endereco depois
+            ; colocando o primeiro numero (base) em ebx para trocar os dados de endereco depois
             mov ebx, eax
 
             push msg_numero
             call printstr
-            ; ler segundo numero
+            ; ler segundo numero (exp)
             push aux_str
             call read32
 
-            ; troca troca jequiti - trocando os operandos de lugar pra fazer a divisao direito
+            ; troca troca jequiti - trocando os operandos de lugar pra fazer a exponenciacao direito
 
-            mov [ebp-4], eax        ; guarda o primeiro numero (dividendo) como variavel local
-            mov eax, ebx            ; o segundo numero (divisor) estara em eax
+            mov [ebp-4], eax        ; guarda o segundo numero (exp) como variavel local
+            mov eax, ebx            ; o primeiro numero (base) estara em eax
 
-            mov esi, 0
+            mov esi, 1              ; iniciando o contador em 1, ja que tem base^1 em eax
 
-loop_exp32: imul [ebp-4], [ebp-4]
+loop_exp32: imul eax
             jo exp_of
             inc esi
-            cmp esi, eax
+            cmp esi, [ebp-4]
             jne loop_exp32
 
-            mov eax, [ebp-4]        ; agora a resposta ta em eax
 
             ; imprime resultado
             push msg_resultado
@@ -63,7 +60,7 @@ exp16:      ; 16 bits
             ; ler numero usuario
             push aux_str
             call read16
-            ; colocando o primeiro numero em ebx para trocar os dados de endereco depois
+            ; colocando o primeiro numero (base) em bx para trocar os dados de endereco depois
             mov bx, ax
 
             push msg_numero
@@ -74,18 +71,16 @@ exp16:      ; 16 bits
 
             ; troca troca jequiti - trocando os operandos de lugar pra fazer a divisao direito
 
-            mov [ebp-2], ax        ; guarda o primeiro numero (dividendo) como variavel local
-            mov ax, bx            ; o segundo numero (divisor) estara em eax
+            mov [ebp-2], ax        ; guarda o segundo numero (exp) como variavel local
+            mov ax, bx             ; o primeiro numero (base) estara em ax
 
-            mov esi, 0
+            mov esi, 1             ; iniciando o contador em 1, ja que tem base^1 em ax
 
-loop_exp16: imul [ebp-2], [ebp-2]
+loop_exp16: imul ax
             jo exp_of
             inc esi
-            cmp esi, ax
+            cmp esi, [ebp-2]
             jne loop_exp16
-
-            mov ax, [ebp-2]        ; agora a resposta ta em eax
 
             ; imprime resultado
             push msg_resultado
@@ -101,7 +96,3 @@ loop_exp16: imul [ebp-2], [ebp-2]
 exp_of:     push msg_overflow
             call printstr
             jmp exit
-
-
-
-
